12dc209fd7977aada9069429bfe48e6805233eee
Fr13ndSDP <2583067591@qq.com>
2024-03-31 15:13:39 +0800

    add slurm script

---
+++ sub.sh
@@ -0,0 +1,18 @@
+# ON XDSC0029
+# DCU(AMD gfx906) + Hygon C86
+
+#!/bin/bash
+#SBATCH -J Flame3D
+#SBATCH -n 16
+#SBATCH -N 4
+#SBATCH --gres=dcu:4
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks-per-socket=1
+#SBATCH -p blcy
+#SBATCH --mem=64G
+
+module purge; 
+module load dtk/23.04.1 compiler/cmake/3.24.1 compiler/devtoolset/7.3.1 mpi/hpcx/2.7.4/gcc-7.3.1
+
+mpirun -np 16 julia run.jl
