182067b760a9997fa036804c42780ba15085882f
Fr13ndSDP <2583067591@qq.com>
2024-03-31 21:08:53 +0800

    fix plot.jl

--- Utils/plot.jl
+++ Utils/plot.jl
@@ -6,7 +6,7 @@ using DelimitedFiles
 
 # const NG::Int64 = h5read("../metrics.h5", "NG")
 
-fname = "../PLT/avg0-50000.vts"
+fname = "../avg-50000.pvts"
 
 plt = pyimport("matplotlib.pyplot")
 plt.rc("text", usetex= false)
@@ -14,7 +14,7 @@ plt.rc("font", family= "sans-serif")
 # plt.rc("font", sans-serif = "Helvetica")
 plt.rc("font", size=15)
 
-vtk = VTKFile(fname)
+vtk = PVTKFile(fname)
 
 # point data
 p_data = get_point_data(vtk)
@@ -55,9 +55,9 @@ id2 = partialsortperm(abs.(uy.-u∞*0.99), 1)
 δ = y[id, id2, 1]
 
 # pw plot
-dns = readdlm("../SCU-benchmark/dns-p.txt", ',', Float32)
+dns = readdlm("../SCU-benchmark/dns-p.dat", ',', Float32)
 plt.subplot(2, 2, 1)
-plt.plot(x1d[1:Nx-20]./δ, pw1d[1:Nx-20])
+plt.plot(x1d[1:Nx-50]./δ, pw1d[1:Nx-50])
 plt.plot(dns[:, 1], dns[:, 2], "+")
 plt.xlim([-10, 6])
 plt.xlabel(raw"$x/\delta$")
@@ -66,7 +66,7 @@ plt.title("pw")
 # plt.show()
 
 # u plot at -30mm
-dns = readdlm("../SCU-benchmark/dns-u.txt", ',', Float32)
+dns = readdlm("../SCU-benchmark/dns-u.dat", ',', Float32)
 plt.subplot(2,2,2)
 yy = y[id, :, 1]./δ
 plt.plot(yy, uy/u∞)
@@ -117,9 +117,9 @@ for i = idcorner+1:Nx
     cf[i] = μ*(u1d[i]*cos(24/180*π) + v1d[i]*sin(24/180*π))/((y[i, 2, 1]-y[i, 1, 1])/cos(24/180*π))/(0.5*ρ∞*u∞^2)
 end
 
-dns = readdlm("../SCU-benchmark/dns-cf.txt", ',', Float32)
+dns = readdlm("../SCU-benchmark/dns-cf.dat", ',', Float32)
 plt.subplot(2,2,3)
-plt.plot(x1d[1:Nx-20]*1000, cf[1:Nx-20])
+plt.plot(x1d[1:Nx-50]*1000, cf[1:Nx-50])
 plt.plot(dns[:, 1], dns[:, 2], "+")
 plt.xlabel(raw"$x/mm$")
 plt.ylabel(raw"$C_f$")
@@ -129,8 +129,7 @@ plt.ylabel(raw"$C_f$")
 id = partialsortperm(abs.(x1d.+0.1), 1)
 
 ρy = zeros(Float32, Ny)
-nu = zeros(Float32, Ny)
-uplus = zeros(Float32, Ny)
+mu = zeros(Float32, Ny)
 uplus_d = zeros(Float32, Ny)
 yplus = zeros(Float32, Ny)
 
@@ -138,33 +137,33 @@ for j = 1:Ny
     uy[j] = sum(u[id, j, :])/Nz
     ρy[j] = sum(ρ[id, j, :])/Nz
     tmp = sum(T[id, j, :])/Nz
-    nu[j] = (1.458f-6*tmp*sqrt(tmp)/(tmp+110.4f0))/ρy[j]
+    mu[j] = (1.458f-6*tmp*sqrt(tmp)/(tmp+110.4f0))
 end
 
 ρw = ρy[1]
-τw = (ρy[1]*nu[1] + ρy[2]*nu[2])/2 * uy[2]/y[id, 2, 1]
+τw = (mu[1]+mu[2])/2 * uy[2]/y[id, 2, 1]
 for j = 1:Ny
-    yplus[j] = y[id, j, 1]*sqrt(τw/ρy[j])/nu[j]
-    uplus[j] = uy[j]/sqrt(τw/ρy[j])
+    yplus[j] = y[id, j, 1]*sqrt(τw/ρw)*ρw/mu[1]
 end
 
-# van driest transform
-for j = 2:Ny
-    for jj = 2:j
-        uplus_d[j] += sqrt((ρy[jj]+ρy[jj-1])/2/ρw)*(uplus[jj]-uplus[jj-1])
+let uvd = 0.f0
+    # van driest transform
+    for j = 2:Ny
+        uvd += (sqrt(ρy[j]/ρw) + sqrt(ρy[j-1]/ρw))/2*(uy[j]-uy[j-1])
+        uplus_d[j] = uvd/sqrt(τw/ρw)
     end
 end
 
 plt.subplot(2,2,4)
-plt.plot(yplus, uplus)
+plt.plot(yplus, uplus_d)
 plt.plot(yplus, yplus)
-plt.plot(yplus, 1/0.41*log.(yplus).+6.0)
-plt.legend([raw"$u^+$", raw"$y^+$", raw"$\frac{1}{0.41}\log y^++6$"])
-plt.ylim([0, 35])
+plt.plot(yplus, 1/0.41*log.(yplus).+5.1)
+plt.legend([raw"$u^+$", raw"$y^+$", raw"$\frac{1}{0.41}\log y^++5.1$"])
+plt.ylim([0, 25])
 plt.xscale("log")
 plt.xlabel(raw"$y^+$")
 plt.ylabel(raw"$u^+_{vd}$")
-plt.title("-100mm, u profile")
+plt.title("-30mm, u profile")
 plt.tight_layout()
 
 # schlieren
@@ -183,67 +182,54 @@ function CD2_R(f)
     return fₓ
 end
 
-# fid = h5open("../metrics.h5", "r")
-# dξdx = fid["dξdx"][NG+1:Nx+NG, NG+1:Ny+NG, 1]
-# dξdy = fid["dξdy"][NG+1:Nx+NG, NG+1:Ny+NG, 1]
-# dηdx = fid["dηdx"][NG+1:Nx+NG, NG+1:Ny+NG, 1]
-# dηdy = fid["dηdy"][NG+1:Nx+NG, NG+1:Ny+NG, 1]
-# close(fid)
+fid = h5open("../metrics.h5", "r")
+dξdx = fid["dξdx"][NG+1:Nx+NG, NG+1:Ny+NG, 1]
+dξdy = fid["dξdy"][NG+1:Nx+NG, NG+1:Ny+NG, 1]
+dηdx = fid["dηdx"][NG+1:Nx+NG, NG+1:Ny+NG, 1]
+dηdy = fid["dηdy"][NG+1:Nx+NG, NG+1:Ny+NG, 1]
+close(fid)
 
-# ρ2d = @view ρ[:, :, 1]
-# ρξ = similar(ρ2d)
-# ρη = similar(ρ2d)
-# ρx = similar(ρ2d)
-# ρy = similar(ρ2d)
-# ∇ρ = similar(ρ2d)
+ρ2d = @view ρ[:, :, 1]
+ρξ = similar(ρ2d)
+ρη = similar(ρ2d)
+ρx = similar(ρ2d)
+ρy = similar(ρ2d)
+∇ρ = similar(ρ2d)
 
-# for j = 1:Ny, i = 4:Nx-3
-#     ρξ[i, j] = CD6(ρ2d[i-3:i+3, j]) 
-# end
+for j = 1:Ny, i = 4:Nx-3
+    ρξ[i, j] = CD6(ρ2d[i-3:i+3, j]) 
+end
 
-# for j = 1:Ny, i = 1:3
-#     ρξ[i, j] = CD2_L(ρ2d[i:i+2, j])
-# end
+for j = 1:Ny, i = 1:3
+    ρξ[i, j] = CD2_L(ρ2d[i:i+2, j])
+end
 
-# for j = 1:Ny, i = Nx-2:Nx
-#     ρξ[i, j] = CD2_R(ρ2d[i-2:i, j])
-# end
+for j = 1:Ny, i = Nx-2:Nx
+    ρξ[i, j] = CD2_R(ρ2d[i-2:i, j])
+end
 
-# for j = 4:Ny-3, i = 1:Nx
-#     ρη[i, j] = CD6(ρ2d[i, j-3:j+3])
-# end
+for j = 4:Ny-3, i = 1:Nx
+    ρη[i, j] = CD6(ρ2d[i, j-3:j+3])
+end
 
-# for j = 1:3, i = 1:Nx
-#     ρη[i, j] = CD2_L(ρ2d[i, j:j+2])
-# end
+for j = 1:3, i = 1:Nx
+    ρη[i, j] = CD2_L(ρ2d[i, j:j+2])
+end
 
-# for j = Ny-2:Ny, i = 1:Nx
-#     ρη[i, j] = CD2_R(ρ2d[i, j-2:j])
-# end
+for j = Ny-2:Ny, i = 1:Nx
+    ρη[i, j] = CD2_R(ρ2d[i, j-2:j])
+end
 
-# @. ρx = ρξ*dξdx + ρη*dηdx
-# @. ρy = ρξ*dξdy + ρη*dηdy
-# @. ∇ρ = sqrt(ρx^2+ρy^2)
+@. ρx = ρξ*dξdx + ρη*dηdx
+@. ρy = ρξ*dξdy + ρη*dηdy
+@. ∇ρ = sqrt(ρx^2+ρy^2)
 
-# max∇ρ = maximum(∇ρ)
-# min∇ρ = minimum(∇ρ)
+max∇ρ = maximum(∇ρ)
+min∇ρ = minimum(∇ρ)
 
-# @. ∇ρ = 0.8*exp(-10*(∇ρ-max∇ρ)/(max∇ρ-min∇ρ))
+@. ∇ρ = 0.8*exp(-10*(∇ρ-max∇ρ)/(max∇ρ-min∇ρ))
 
 plt.figure()
-plt.contourf(x[:, :, 1], y[:, :, 1], T[:, :, 1], 100, cmap="coolwarm")
+plt.contourf(x[:, :, 1], y[:, :, 1], ∇ρ[:, :, 1], 100, cmap="coolwarm")
 plt.axis("equal")
 plt.show()
-# fig = plt.figure(figsize=(18, 6))
-# # lev= np.linspace(1e-6, 0.27, 60)
-# plt.contour(x[1:Nx-20, :, cld(Nz, 2)], y[1:Nx-20, :, cld(Nz, 2)], YH2O[1:Nx-20, :, cld(Nz, 2)], levels=60, cmap="hot", extend="min")
-# a = plt.contourf(x[1:Nx-20, :, cld(Nz, 2)], y[1:Nx-20, :, cld(Nz, 2)], T[1:Nx-20, :, cld(Nz, 2)], 60, cmap="coolwarm", extend="min")
-# plt.xlabel(raw"$x/m$")
-# plt.ylabel(raw"$y/m$")
-
-# plt.colorbar(a, label=raw"$T$", location="right")
-# plt.annotate(raw"$H_2O$ mass fraction", style="italic", xy=(0.015,-0.001), xytext=(0.01,-0.003), arrowprops=Dict("facecolor"=>"black"))
-# # plt.tight_layout()
-# plt.title("5 million grid, H2 combustion with one GPU")
-# # plt.show()
-# plt.savefig("view.png", dpi=600)
