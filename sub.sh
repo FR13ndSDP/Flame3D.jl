9f9a63cd27c23f440bbced2c8f734b72a043e821
Fr13ndSDP <2583067591@qq.com>
2024-04-10 17:22:53 +0800

    update slurm script

--- sub.sh
+++ sub.sh
@@ -8,6 +8,8 @@
 #SBATCH --ntasks-per-socket=1
 #SBATCH -p blcy
 #SBATCH --mem=64G
+#SBATCH -o slurm-out
+#SBATCH -e slurm-err
 
 module purge; 
 module load dtk/23.04.1 compiler/cmake/3.24.1 compiler/devtoolset/7.3.1 mpi/hpcx/2.7.4/gcc-7.3.1
