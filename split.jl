3f65a99cfa0e182262acef3681dd28a824bf8c36
Fr13ndSDP <2583067591@qq.com>
2024-05-17 23:48:15 +0800

    add vanLeer and AUSM

--- split.jl
+++ split.jl
@@ -106,3 +106,151 @@ function fluxSplit_LF(Q, Fp, Fm, S, Ax, Ay, Az)
     @inbounds Fm[i, j, k, 5] = 0.5f0*((E0+p)*un - λ0*E0)
     return 
 end
+
+function fluxSplit_VL(Q, Fp, Fm, S, Ax, Ay, Az)
+    i = (blockIdx().x-1i32)* blockDim().x + threadIdx().x
+    j = (blockIdx().y-1i32)* blockDim().y + threadIdx().y
+    k = (blockIdx().z-1i32)* blockDim().z + threadIdx().z
+
+    if i > Nxp+2*NG || j > Nyp+2*NG || k > Nzp+2*NG
+        return
+    end
+
+    @inbounds ρ = Q[i, j, k, 1]
+    @inbounds u = Q[i, j, k, 2]
+    @inbounds v = Q[i, j, k, 3]
+    @inbounds w = Q[i, j, k, 4]
+    @inbounds p = Q[i, j, k, 5]
+
+    @inbounds A1 = Ax[i, j, k]
+    @inbounds A2 = Ay[i, j, k]
+    @inbounds A3 = Az[i, j, k]
+    @inbounds ss = S[i, j, k]
+
+    @fastmath c = sqrt(γ*p/ρ)
+
+    un = A1*u + A2*v + A3*w
+    E0 = p/(γ-1) + 0.5f0*ρ*(u^2+v^2+w^2)
+    
+    M = un/(c*ss)
+
+    if M <= -1
+        @inbounds Fp[i, j, k, 1] = 0.f0
+        @inbounds Fp[i, j, k, 2] = 0.f0
+        @inbounds Fp[i, j, k, 3] = 0.f0
+        @inbounds Fp[i, j, k, 4] = 0.f0
+        @inbounds Fp[i, j, k, 5] = 0.f0
+    
+        @inbounds Fm[i, j, k, 1] = ρ * un
+        @inbounds Fm[i, j, k, 2] = ρ * u * un + A1 * p
+        @inbounds Fm[i, j, k, 3] = ρ * v * un + A2 * p
+        @inbounds Fm[i, j, k, 4] = ρ * w * un + A3 * p
+        @inbounds Fm[i, j, k, 5] = (E0 + p) * un
+    elseif M >= 1
+        @inbounds Fm[i, j, k, 1] = 0.f0
+        @inbounds Fm[i, j, k, 2] = 0.f0
+        @inbounds Fm[i, j, k, 3] = 0.f0
+        @inbounds Fm[i, j, k, 4] = 0.f0
+        @inbounds Fm[i, j, k, 5] = 0.f0
+    
+        @inbounds Fp[i, j, k, 1] = ρ * un
+        @inbounds Fp[i, j, k, 2] = ρ * u * un + A1 * p
+        @inbounds Fp[i, j, k, 3] = ρ * v * un + A2 * p
+        @inbounds Fp[i, j, k, 4] = ρ * w * un + A3 * p
+        @inbounds Fp[i, j, k, 5] = (E0 + p) * un
+    else
+        un /= ss
+        f1p = ρ * c * 0.25f0 *(M + 1)^2 * ss
+        f1m = -ρ * c * 0.25f0 *(M - 1)^2 * ss
+        tmp1 = (-un+2*c)/(γ*ss)
+        tmp2 = (-un-2*c)/(γ*ss)
+        tmp3 = 1/(2*(γ^2-1))
+        tmp4 = 0.5f0*(u^2+v^2+w^2-un^2)
+
+        @inbounds Fp[i, j, k, 1] = f1p
+        @inbounds Fp[i, j, k, 2] = f1p*(A1*tmp1 + u)
+        @inbounds Fp[i, j, k, 3] = f1p*(A2*tmp1 + v)
+        @inbounds Fp[i, j, k, 4] = f1p*(A3*tmp1 + w)
+        @inbounds Fp[i, j, k, 5] = f1p*(((γ-1)*un+2*c)^2*tmp3+tmp4)
+    
+        @inbounds Fm[i, j, k, 1] = f1m
+        @inbounds Fm[i, j, k, 2] = f1m*(A1*tmp2 + u)
+        @inbounds Fm[i, j, k, 3] = f1m*(A2*tmp2 + v)
+        @inbounds Fm[i, j, k, 4] = f1m*(A3*tmp2 + w)
+        @inbounds Fm[i, j, k, 5] = f1m*(((γ-1)*un-2*c)^2*tmp3+tmp4)
+    end
+    return
+end
+
+function fluxSplit_AUSM(Q, Fp, Fm, S, Ax, Ay, Az)
+    i = (blockIdx().x-1i32)* blockDim().x + threadIdx().x
+    j = (blockIdx().y-1i32)* blockDim().y + threadIdx().y
+    k = (blockIdx().z-1i32)* blockDim().z + threadIdx().z
+
+    if i > Nxp+2*NG || j > Nyp+2*NG || k > Nzp+2*NG
+        return
+    end
+
+    @inbounds ρ = Q[i, j, k, 1]
+    @inbounds u = Q[i, j, k, 2]
+    @inbounds v = Q[i, j, k, 3]
+    @inbounds w = Q[i, j, k, 4]
+    @inbounds p = Q[i, j, k, 5]
+
+    @inbounds A1 = Ax[i, j, k]
+    @inbounds A2 = Ay[i, j, k]
+    @inbounds A3 = Az[i, j, k]
+    @inbounds ss = S[i, j, k]
+
+    @fastmath c = sqrt(γ*p/ρ)
+
+    un = A1*u + A2*v + A3*w
+    h = γ*p/(ρ*(γ-1)) + 0.5f0*(u^2+v^2+w^2)
+    
+    M = un/(c*ss)
+
+    if M <= -1
+        @inbounds Fp[i, j, k, 1] = 0.f0
+        @inbounds Fp[i, j, k, 2] = 0.f0
+        @inbounds Fp[i, j, k, 3] = 0.f0
+        @inbounds Fp[i, j, k, 4] = 0.f0
+        @inbounds Fp[i, j, k, 5] = 0.f0
+    
+        @inbounds Fm[i, j, k, 1] = ρ * un
+        @inbounds Fm[i, j, k, 2] = ρ * u * un + A1 * p
+        @inbounds Fm[i, j, k, 3] = ρ * v * un + A2 * p
+        @inbounds Fm[i, j, k, 4] = ρ * w * un + A3 * p
+        @inbounds Fm[i, j, k, 5] = ρ * h * un
+    elseif M >= 1
+        @inbounds Fm[i, j, k, 1] = 0.f0
+        @inbounds Fm[i, j, k, 2] = 0.f0
+        @inbounds Fm[i, j, k, 3] = 0.f0
+        @inbounds Fm[i, j, k, 4] = 0.f0
+        @inbounds Fm[i, j, k, 5] = 0.f0
+    
+        @inbounds Fp[i, j, k, 1] = ρ * un
+        @inbounds Fp[i, j, k, 2] = ρ * u * un + A1 * p
+        @inbounds Fp[i, j, k, 3] = ρ * v * un + A2 * p
+        @inbounds Fp[i, j, k, 4] = ρ * w * un + A3 * p
+        @inbounds Fp[i, j, k, 5] = ρ * h * un
+    else
+        fp = 0.25f0*(M + 1)^2*ρ*c*ss
+        fm = -0.25f0*(M - 1)^2*ρ*c*ss
+
+        Pp = p * (0.25f0*(M+1)^2*(2-M))
+        Pm = p * (0.25f0*(M-1)^2*(2+M))
+
+        @inbounds Fp[i, j, k, 1] = fp
+        @inbounds Fp[i, j, k, 2] = fp * u + A1 * Pp
+        @inbounds Fp[i, j, k, 3] = fp * v + A2 * Pp
+        @inbounds Fp[i, j, k, 4] = fp * w + A3 * Pp
+        @inbounds Fp[i, j, k, 5] = fp * h
+    
+        @inbounds Fm[i, j, k, 1] = fm
+        @inbounds Fm[i, j, k, 2] = fm * u + A1 * Pm
+        @inbounds Fm[i, j, k, 3] = fm * v + A2 * Pm
+        @inbounds Fm[i, j, k, 4] = fm * w + A3 * Pm
+        @inbounds Fm[i, j, k, 5] = fm * h
+    end
+    return
+end
